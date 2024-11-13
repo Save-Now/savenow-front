801bed65e066a1af2805bf9a80063a4d5ac58caf
hannah0352 <105264420+hannah0352@users.noreply.github.com>
2024-11-13 15:52:17 +0900

    커밋 이정원

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useEffect, useState } from "react";
 import axios from "axios";
 import theme from "./styles/theme";
@@ -6,13 +5,10 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MyPage from "./pages/MyPage";
 import SignUp from "./pages/SignUp";
 import { ThemeProvider } from "styled-components";
-=======
->>>>>>> 50be831 (Revert "커밋 이정원")
 
 function App() {
 
   return (
-<<<<<<< HEAD
     <>
       <ThemeProvider theme={theme}>
         <Router>
@@ -23,10 +19,6 @@ function App() {
         <SignUp />
       </ThemeProvider>
     </>
-=======
-    <div className="App">
-    </div>
->>>>>>> 50be831 (Revert "커밋 이정원")
   );
 }
 
