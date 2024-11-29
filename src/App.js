12f9c04c02d52c9be87d8d36f1f6a06fab3629df
hannah0352 <105264420+hannah0352@users.noreply.github.com>
2024-11-29 17:17:51 +0900

    마이 페이지 디자인

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import SignUp from './component/SignUp';
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MyPage from "./pages/MyPage";
 
 function App() {
   const [hello, setHello] = useState("");
@@ -14,18 +14,17 @@ function App() {
       })
       .catch((error) => {
         // 에러 처리 추가
-        console.error("Error:", error)
+        console.error("Error:", error);
       });
   }, []); // 의존성 배열을 여기로 이동
 
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/mypage" element={<MyPage />}/>
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      {/* <div className="App">백엔드 데이터: {hello}</div> */}
+      <Routes>
+        <Route path="/mypage" element={<MyPage />} />
+      </Routes>
+    </Router>
   );
 }
 
