4f83b8fcb65ea102732c3ab62b1cefea3fc982da
suyeon <jso8787@naver.com>
2024-11-30 16:22:54 +0900

    [#8] pr conflict 수정

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import axios from 'axios';
 import SignUp from './component/SignUp';
 
 function App() {
-  const [hello, setHello] = useState('');
+  const [hello, setHello] = useState("");
   useEffect(() => {
-    axios.get('http://localhost:8080/api/test')
+    axios
+      .get("http://localhost:8080/api/test")
       .then((res) => {
         setHello(res.data);
-      }, []);
-  })
-  .catch((error) => {
-    // 에러 처리 추가
-    console.error("Error:", error)
+      })
+      .catch((error) => {
+        // 에러 처리 추가
+        console.error("Error:", error)
+      });
   }, []); // 의존성 배열을 여기로 이동
-  
+
   return (
     <>
-    <SignUp/>
+      <SignUp />
     </>
   );
 }
