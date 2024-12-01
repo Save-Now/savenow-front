0f820555c4b87cc72fcdc0bdee69719d0c3a266e
suyeon <jso8787@naver.com>
2024-12-02 01:08:13 +0900

    [#3] 중복체크, 폼 제출 제외 기능 구현

--- src/component/SignUp.jsx
+++ src/component/SignUp.jsx
@@ -3,14 +3,17 @@ import { useState, useEffect, useRef } from 'react';
 import { useImmer } from 'use-immer';
 import axios from 'axios';
 
+// const BASE_URL = 'http://localhost:8080'
 const BASE_URL = "https://71c13204-cc41-4566-a89f-cf9b87467725.mock.pstmn.io" //mock서버
 
-const msg = {
+const MSG = {
+    noWarn: 'noWarn',
     required: '*필수입력 항목입니다.',
     emailDupli: '*중복된 이메일입니다.',
-    nameDupli: '*중복된 닉네임입니다.',
+    usernameDupli: '*중복된 닉네임입니다.',
     emailPattern: '*잘못된 이메일 형식입니다.',
     pwdPattern: '*비밀번호는 8자리 이상, 숫자와 영문자를 포함해야 합니다.',
+    usernamePattern: '*닉네임은 25자리 이하여야 합니다.',
     pwdConfirm: '*비밀번호가 일치하지 않습니다.',
 };
 
@@ -18,128 +21,189 @@ const EMAIL_REG = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
 const PWD_REG = /(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
 
 export default function SignUp() {
+    // Todo: birth 필요
     const [formData, updateFormData] = useImmer({
         email: '',
         password: '',
-        name: '',
+        passwordConfirm: '',
+        username: '',
+        birth: '',
+        gender: 'man',  // default 남자
         profileImg: '',
-        gender: ''
     });
     const [warnMsg, updateWarnMsg] = useImmer({
         email: '',
         password: '',
         passwordConfirm: '',
-        name: '',
+        username: '',
+        birth: '',
         profileImg: '',
-        gender: 'noWarn'
+        // gender는 default 값때메 제거
     });
     const [gender, updateGender] = useImmer({ man: true, woman: false })
-    const [passwordConfirm, setPasswordConfirm] = useState('');
     const [isActivated, setIsActivated] = useState(false);
 
+    const focusRef = useRef(null);
+    const submitRef = useRef(null);
+
+    // 회원가입 제출 버튼 활성화
     useEffect(() => {
-        if (Object.values(warnMsg).every(value => value === 'noWarn')) {
+        console.log(warnMsg);
+        if (Object.values(warnMsg).every(value => value === MSG.noWarn)) {
+            console.log('warnMsg 업뎃');
             setIsActivated(true);
         }
     }, [warnMsg])
 
-
-
-    // 비밀번호 재확인
-    useEffect(() => {
-        confirmPassword();
-    }, [passwordConfirm]);
-
-    // 이메일, 닉네임 패턴 확인
-    useEffect(() => {
-        if (formData.email !== prevEmail.current) {
-            checkPattern('email')
-        }
-        if (formData.password !== prevPwd.current) {
-            checkPattern('password');
-        }
-    }, [formData.email, formData.password])
-
-    // 이메일, 닉네임의 이전값
-    const prevEmail = useRef(formData.email);
-    const prevPwd = useRef(formData.password);
-
-    // 상태값 변경
-    const handleValue = e => {
-        let name = e.target.name;
-        let value = e.target.value;
-        if (name === 'passwordConfirm') {
-            setPasswordConfirm(value.trim());
-        } else {
-            updateFormData(draft => {
-                draft[name] = value.trim();
-            })
-        };
+    const handleValue = (e) => {
+        focusRef.current = e.target;
+        const name = e.target.name;
+        const value = e.target.value;
+        updateFormData(draft => {
+            draft[name] = value.trim();
+        }); // 비동기 작업
     };
 
-    // 확인용
+    // 유효성 검사
     useEffect(() => {
-        console.log(formData);
+        validate();
     }, [formData])
 
-    useEffect(() => {
-        console.log(warnMsg);
-    }, [warnMsg])
-
-    // 필수필드 입력 유무 확인(onBlur? onChange?)
-    // Todo: onChange로 바꾸기
-    const checkRequired = e => {
-        let name = e.target.name;
-        if (name === 'passwordConfirm' && passwordConfirm === '') {
-            updateWarnMsg(draft => {
-                draft[name] = msg.required;
-            })
-            return;
-        } else if (name !== passwordConfirm && formData[name] === '') {
-            updateWarnMsg(draft => {
-                draft[name] = msg.required;
-            })
-            return;
+    const validate = () => {
+        let target;
+        let value;
+        if (focusRef.current) {
+            target = focusRef.current.name;
+            value = formData[target];
         } else {
-            console.log('dsf');
-            console.log(name);
+            target = '';
+            value = '';
+        }
+        if (target === 'gender')
+            if (!checkRequired(target)) {
+                return;
+            };
+        switch (target) {
+            case 'email':
+            case 'username':
+                if (!checkPattern(target)) {
+                    return;
+                } else {
+                    if (!checkDuplication(target)) {
+                        return;
+                    }
+                }
+                break;
+            case 'password':
+                if (!checkPattern(target)) {
+                    return;
+                }
+                break;
+            case 'passwordConfirm':
+                if (!confirmPassword(value)) {
+                    return;
+                }
+                break;
+        }
+        updateWarnMsg(draft => {
+            draft[target] = MSG.noWarn;
+        })
+    }
+
+    // 필수 입력 여부 확인
+    const checkRequired = (name) => {
+        if (formData[name] === '') {
             updateWarnMsg(draft => {
-                draft[name] = 'noWarn';
+                draft[name] = MSG.required;
             })
+            return false;
+        } else {
+            return true;
         }
     };
-    // 패턴 검사(onChange)
+
+    // 패턴 확인
     const checkPattern = (name) => {
-        // Todo: checkRequired 호출 후 checkPattern 할 수 있도록 수정
         let result;
         if (name === 'email') {
             result = EMAIL_REG.test(formData.email);
-            updateWarnMsg(draft => {
-                draft.email = result ? 'noWarn' : msg.emailPattern;
-            });
-        } else if (name === 'password') {
+            if (!result) {
+                updateWarnMsg(draft => {
+                    draft[name] = MSG.emailPattern;
+                });
+                return false;
+            } else {
+                console.log('패턴확인 통과');
+                return true;
+            }
+        }
+        if (name === 'password') {
             result = PWD_REG.test(formData.password);
-            updateWarnMsg(draft => {
-                draft.password = result ? 'noWarn' : msg.pwdPattern;
-            });
-        } else {
-            console.log('checkPatternError: target이 잘못됨');
+            if (!result) {
+                updateWarnMsg(draft => {
+                    draft[name] = MSG.pwdPattern;
+                });
+                return false;
+            } else {
+                console.log('패턴확인 통과')
+                return true;
+            }
+        }
+        if (name === 'username') {
+            result = formData.username.length <= 25;
+            if (!result) {
+                updateWarnMsg(draft => {
+                    draft[name] = MSG.usernamePattern;
+                });
+                return false;
+            } else {
+                console.log('패턴확인 통과')
+                return true;
+            }
         }
     };
-    // 비밀번호 재확인(onChange)
-    const confirmPassword = () => {
-        if (passwordConfirm && formData.password !== passwordConfirm) {
+
+    // 비번 재확인
+    const confirmPassword = (value) => {
+        if (value !== formData.password) {
             updateWarnMsg(draft => {
-                draft.passwordConfirm = msg.pwdConfirm;
-            });
-        } else if (passwordConfirm && formData.password === passwordConfirm) {
-            updateWarnMsg(draft => {
-                draft.passwordConfirm = 'noWarn';
+                draft.passwordConfirm = MSG.pwdConfirm;
             })
+            return false;
         } else {
-            console.log(passwordConfirm);
+            console.log('패스워드 재확인 통과')
+            return true;
         }
-    };
+    }
+
+    // 중복 체크(mock서버 임시코드)
+    // Todo: api 설계후 수정 예정
+    const checkDuplication = async (name) => {
+        try {
+            let params = {}
+            if (name === 'email') params.email = formData.email;
+            if (name === 'username') params.username = formData.username;
+            const response = await axios.get(`${BASE_URL}/api/join/userNa?${name}="${params[name]}"`);  //Todo: axios params 따옴표 인코딩
+            const message = response.data.message;
+            if (message) {
+                if (message === 'duplication') {
+                    updateWarnMsg(draft => {
+                        draft[name] = name === 'email' ? MSG.emailDupli : MSG.usernameDupli;
+                    })
+                    return false;
+                } else if (message === 'not duplication') {
+                    console.log('중복 통과');
+                    return true;
+                }
+            } else {
+                console.log(`no message: ${message}`);
+                return false;
+            }
+        } catch (error) {
+            console.error('Error checking duplication: ', error.message);
+        }
+    }
+
     // 성별 checked 바꾸기
     const handleGenderChecked = (e) => {
         if (e.target.value === 'man') {
@@ -163,38 +227,14 @@ export default function SignUp() {
         }
     }
 
-    // 중복 검사(onBlur)
-    // mock서버로 임시 구현
-    const checkDuplication = async (e) => {
+    // Todo: 회원가입 데이터 post
+    const handleSubmit = async() => {
         try {
-            checkRequired(e);    // 입력 여부 체크
-            const name = e.target.name;
-            let params = {};
-            if (name === 'email') params.email = formData.email;
-            if (name === 'name') params.name = encodeURIComponent(formData.name);
-            const response = await axios.get(`${BASE_URL}/api/auth/users/?${name}="${params.email}"`);  //Todo: axios params 따옴표 인코딩
-            let message = response.data.message;
-            if (message) {
-                if (message === 'duplication') {
-                    updateWarnMsg(draft => {
-                        draft[name] = name === 'email' ? msg.emailDupli : msg.nameDupli;
-                    })
-                } else if (message === 'not duplication') {
-                    updateWarnMsg(draft => {
-                        draft[name] = 'noWarn';
-                    })
-                } else {
-                    console.log(`message: ${message}`);
-                }
-            } else {
-                console.log(`no message: ${message}`);
-            }
-        } catch (error) {
-            console.error('Error checking duplication: ', error.message);
-            return { suceess: false, message: 'failed to check duplication' };
-        };
-    }
 
+        } catch (error) {
+
+        }
+    }
 
     return (
         <div className={styles.container}>
@@ -213,14 +253,12 @@ export default function SignUp() {
                         required
                         value={formData.email}
                         onChange={handleValue}
-                        onBlur={checkDuplication}
                         placeholder='아이디를 입력해주세요.'
                     />
                 </label><br />
-                {warnMsg.email === msg.required && <span className={styles.warnMsg}>{msg.required}</span>}
-                {warnMsg.email === msg.emailPattern && <span className={styles.warnMsg}>{msg.emailPattern}</span>}
-                {warnMsg.email === msg.emailDupli && <span className={styles.warnMsg}>{msg.emailDupli}</span>}
-                {warnMsg.email === 'noWarn' && <span className={styles.warnMsg}>{''}</span>}
+                {warnMsg.email === MSG.required && <span className={styles.warnMsg}>{MSG.required}</span>}
+                {warnMsg.email === MSG.emailPattern && <span className={styles.warnMsg}>{MSG.emailPattern}</span>}
+                {warnMsg.email === MSG.emailDupli && <span className={styles.warnMsg}>{MSG.emailDupli}</span>}
                 <br />
                 <label>
                     비밀번호<br />
@@ -231,14 +269,13 @@ export default function SignUp() {
                         required
                         value={formData.password}
                         onChange={handleValue}
-                        onBlur={checkRequired}
                         placeholder='비밀번호를 입력해주세요.'
                     />
                 </label>
                 <br />
-                {warnMsg.password === msg.required && <span className={styles.warnMsg}>{msg.required}</span>}
-                {warnMsg.password === msg.pwdPattern && <span className={styles.warnMsg}>{msg.pwdPattern}</span>}
-                <span>{warnMsg === `pwdMsg` ? msg.pwdMsg : ''}</span><br />
+                {warnMsg.password === MSG.required && <span className={styles.warnMsg}>{MSG.required}</span>}
+                {warnMsg.password === MSG.pwdPattern && <span className={styles.warnMsg}>{MSG.pwdPattern}</span>}
+                <br />
                 <label>
                     비밀번호 재확인<br />
                     <input
@@ -246,66 +283,82 @@ export default function SignUp() {
                         type='password'
                         name='passwordConfirm'
                         required
-                        value={passwordConfirm}
+                        value={formData.passwordConfirm}
                         onChange={handleValue}
-                        onBlur={checkRequired}
                         placeholder='비밀번호를 재입력해주세요.'
                     />
                 </label>
                 <br />
-                {warnMsg.passwordConfirm === msg.required && <span className={styles.warnMsg}>{msg.required}</span>}
-                {warnMsg.passwordConfirm === msg.pwdConfirm && <span className={styles.warnMsg}>{msg.pwdConfirm}</span>}
+                {warnMsg.passwordConfirm === MSG.required && <span className={styles.warnMsg}>{MSG.required}</span>}
+                {warnMsg.passwordConfirm === MSG.pwdConfirm && <span className={styles.warnMsg}>{MSG.pwdConfirm}</span>}
                 <br />
                 <label>
                     닉네임<br />
                     <input
                         className={styles.input}
                         type='text'
-                        name='name'
-                        value={formData.name}
+                        name='username'
+                        value={formData.username}
                         onChange={handleValue}
-                        onBlur={checkDuplication}
                         placeholder='닉네임을 입력해주세요.'
                     />
                 </label>
                 <br />
-                {warnMsg.name === msg.required && <span className={styles.warnMsg}>{msg.required}</span>}
-                {warnMsg.name === msg.nameDupli && <span className={styles.warnMsg}>{msg.nameDupli}</span>}
+                {warnMsg.username === MSG.required && <span className={styles.warnMsg}>{MSG.required}</span>}
+                {warnMsg.username === MSG.usernameDupli && <span className={styles.warnMsg}>{MSG.usernameDupli}</span>}
+                {warnMsg.username === MSG.usernamePattern && <span className={styles.warnMsg}>{MSG.usernamePattern}</span>}
+                <br />
+                <label>
+                    생일<br />
+                    <input
+                        className={styles.input}
+                        type='date'
+                        name='birth'
+                        min='1940-01-01'
+                        max='2023-12-31'
+                        onChange={handleValue}
+                    />
+                </label>
+                <br />
+                {warnMsg.username === MSG.required && <span className={styles.warnMsg}>{MSG.required}</span>}
                 <br />
                 <label className={styles.gender}>
                     성별<br />
                     <ul>
                         <li>
-                            <input
-                                type='radio'
-                                name='man'
-                                value='man'
-                                checked={gender.man}
-                                onChange={handleValue}
-                                onClick={handleGenderChecked}
-                            />
-                            <label className={styles.genderLabel} htmlFor='man'>남성</label>
-                            <input
-                                className={styles.gender}
-                                type='radio'
-                                name='woman'
-                                value='woman'
-                                checked={gender.woman}
-                                onChange={handleValue}
-                                onClick={handleGenderChecked}
-                            />
-                            <label className={styles.genderLabel} htmlFor='woman'>여성</label>
+                            <label>
+                                <input
+                                    type='radio'
+                                    name='gender'
+                                    value='man'
+                                    checked={gender.man}
+                                    onChange={handleValue}
+                                    onClick={handleGenderChecked}
+                                />
+                                남성
+                            </label>
+                            <label>
+                                <input
+                                    className={styles.gender}
+                                    type='radio'
+                                    name='gender'
+                                    value='woman'
+                                    checked={gender.woman}
+                                    onChange={handleValue}
+                                    onClick={handleGenderChecked}
+                                />
+                                여성
+                            </label>
                         </li>
                     </ul>
                 </label><br />
-                {/* {warnMsg.gender === msg.required && <span className={styles.warnMsg}>{msg.required}</span>}<br /> */}
                 <label className={styles.fileBox}> 프로필 이미지<br />
                     <div className={styles.input}>
                         <input
                             className={styles.uploadName}
                             value='첨부파일'
                             placeholder='첨부파일'
-
+                            readOnly
                         />
                         <label htmlFor='file'>파일찾기</label>
                         <input
@@ -315,16 +368,17 @@ export default function SignUp() {
                             name='profileImg'
                             value={formData.profileImg}
                             onChange={handleValue}
-                            onBlur={checkRequired}
                         />
                     </div>
                 </label><br />
-                {warnMsg.profileImg === msg.required && <span className={styles.warnMsg}>{msg.emptyMsg}</span>}<br />
+                {warnMsg.profileImg === MSG.required && <span className={styles.warnMsg}>{MSG.emptyMsg}</span>}<br />
                 <input
                     className={isActivated ? `${styles.submit} ${styles.active}` : `${styles.submit}`}
                     type='submit'
                     value='회원가입'
                     readOnly
+                    onClick={handleSubmit}
+                    ref={submitRef}
                 />
             </form>
         </div>
