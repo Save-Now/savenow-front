59fdd92daef19d87fd1fdab31d6dae06c443de56
suyeon <jso8787@naver.com>
2024-12-13 23:15:01 +0900

    Feat: 회원가입 기능 구현
    이메일, 아이디 중복 확인 및 회원가입 제출 폼 개발
    
    Resolves: #8

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from './store/store'
 import { Provider } from 'react-redux'
+import GlobalStyle from './styles/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyle />
       <App />
     </Provider>
   </React.StrictMode>
