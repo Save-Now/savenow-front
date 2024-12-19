807309e7812e8ac1006c9dfef9f25dd04abba92d
Kim GeonHee <kafoo0105@naver.com>
2024-12-19 17:54:56 +0900

    feat: App.js에 로그인 및 대시보드 페이지의 라우트 설정 및 임포트 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-<<<<<<< HEAD
 import { useEffect, useState } from "react";
 import axios from "axios";
 import theme from "./styles/theme";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MyPage from "./pages/MyPage";
 import SignUp from "./pages/SignUp";
+import Login from './pages/Login';
+import Test from './pages/Test';
+import Dashboard from './pages/Dashboard';
 import { ThemeProvider } from "styled-components";
 
 function App() {
@@ -14,27 +16,14 @@ function App() {
         <Router>
           <Routes>
             <Route path="/mypage" element={<MyPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/test" element={<Test />} />
+            <Route path="/dashboard" element={<Dashboard />} />
           </Routes>
         </Router>
         <SignUp />
       </ThemeProvider>
     </>
-=======
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './pages/Login';
-import Test from './pages/Test';
-import Dashboard from './pages/Dashboard';
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/test" element={<Test />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
->>>>>>> 3b1d41d (feat: 대시보드 및 구성 컴포넌트 추가, 차트 라이브러리 설치)
   );
 }
 
