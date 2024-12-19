6adbf4aa2ff65b5085ee4b55699b884cc4162769
Kim GeonHee <kafoo0105@naver.com>
2024-12-19 18:59:28 +0900

    변경사항 수정

--- src/App.js
+++ src/App.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-import { useEffect, useState } from "react";
-import axios from "axios";
-import theme from "./styles/theme";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import MyPage from "./pages/MyPage";
-import SignUp from "./pages/SignUp";
-import { ThemeProvider } from "styled-components";
-
-function App() {
-  return (
-    <>
-      <ThemeProvider theme={theme}>
-        <Router>
-          <Routes>
-            <Route path="/mypage" element={<MyPage />} />
-          </Routes>
-        </Router>
-        <SignUp />
-      </ThemeProvider>
-    </>
-=======
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Test from './pages/Test';
@@ -34,7 +12,6 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
->>>>>>> 3b1d41d (feat: 대시보드 및 구성 컴포넌트 추가, 차트 라이브러리 설치)
   );
 }
 
